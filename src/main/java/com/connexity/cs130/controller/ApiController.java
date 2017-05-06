c19318751162d5f3d3e49c98d6917b22092e5a9d
Dominic Mortel <dmortel@ucla.edu>
2017-05-05 22:57:07 -0700

    Cleaned up some code and renamed files

--- src/main/java/com/connexity/cs130/controller/ApiController.java
+++ src/main/java/com/connexity/cs130/controller/ApiController.java
@@ -1,13 +1,11 @@
 package com.connexity.cs130.controller;
 
-import com.connexity.cs130.model.MerchantsResponse;
 import com.connexity.cs130.model.ProductResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
@@ -17,7 +15,7 @@ import java.util.Map;
  */
 @Controller
 @RequestMapping("/api")
-public class HelloController {
+public class ApiController {
 
     @Autowired
     RestTemplate restTemplate;
@@ -28,35 +26,18 @@ public class HelloController {
     @Value("${publisher.id}")
     Long publisherId;
 
-    @RequestMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Map<String, Object> context) {
-        String message = "Hello " + name;
-        context.put("message", message);
-        return "helloDynamic";
-    }
-
-    @RequestMapping("/catalog")
+    @RequestMapping("/search")
     public String catalog(@RequestParam(name = "keyword") String keyword, Map<String, Object> context) {
         ProductResponse response;
-        String url = createMerchantInfoRequestUrl(keyword);
+        String url = createProductInfoRequestUrl(keyword);
         response = restTemplate.getForEntity(url, ProductResponse.class).getBody();
         context.put("message", response);
         return "helloDynamic";
     }
 
-    /*@RequestMapping(value="/proxy", produces = "Application/json")
-    @ResponseBody
-    public ProductResponse proxy() {
-        ProductResponse response;
-        String url = createMerchantInfoRequestUrl("bed");
-        response = restTemplate.getForEntity(url, ProductResponse.class).getBody();
-
-        return response;
-    }*/
-
-    private String createMerchantInfoRequestUrl(String keyword) {
+    private String createProductInfoRequestUrl(String keyword) {
         String url = "http://catalog.bizrate.com/services/catalog/v1/api/product?apiKey="
-                + "f94ab04178d1dea0821d5816dfb8af8d" + "&publisherId=" + "608865" + "&keyword=" + keyword + "&format=json";
+                + apiKey + "&publisherId=" + publisherId + "&keyword=" + keyword + "&format=json";
         return url;
     }
 }
