2bfbc19b8b6274f1f94357d1d0eb4a4f87fb0144
lynnhaotran <lynn.h.tran@gmail.com>
2017-05-31 09:07:59 -0700

    product page styles updated

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -21,7 +21,9 @@ function productSearchAsc() {
 }
 
 function productSearchDesc() {
-    $('#productSearch').attr('action','/api/searchDesc');
+    $('#productSearch').attr('action','/api/searchDesc')
+
+    $('.desc').addClass('down');
 }
 
 function productNextPage() {
@@ -36,3 +38,25 @@ function idSearch() {
     $('.productPage').attr('action','/api/searchId');
 }
 
+/*Based off the tutorial from https://www.w3schools.com/howto/howto_js_tabs.asp*/
+
+function productTab(evt, condition) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(condition).style.display = "block";
+    evt.currentTarget.className += " active";
+}
