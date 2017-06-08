1b397d7d20dd1250cf5773b5f1adfb29dd9c1ebd
Daniel Ong <daniel_ngo_d@yahoo.com>
2017-06-07 21:16:16 -0700

    Added licensing

--- src/main/java/com/connexity/cs130/controller/ApiController.java
+++ src/main/java/com/connexity/cs130/controller/ApiController.java
@@ -1,3 +1,23 @@
+/*****************************************************************************************************
+ * Your Awesome Stuff (YAS) - Web Application where buyers or sellers can find the price of a product.
+ * Copyright (C) 2017 CS130YAS
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *****************************************************************************************************
+ */
+
 package com.connexity.cs130.controller;
 
 import com.connexity.cs130.amazon.SignedRequestsHelper;
@@ -38,10 +58,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import com.connexity.cs130.model.User;
 import com.connexity.cs130.service.UserService;
 
-
-/**
- * Created by 161497 on 4/20/17.
- */
 @Controller
 public class ApiController {
 
